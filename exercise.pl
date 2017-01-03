5cc45f1a5fc684c77198e78d66413dd7c42a6855
makenyw <1453397214@qq.com>
2017-01-03 19:43:51 +0800

    exercise

--- exercise.pl
+++ exercise.pl
@@ -1,76 +1,51 @@
 #!/usr/bin/perl 
 use strict;
 use warnings;
-my $age;
-my $BMI;
-print "you should do exercise as this\n\n";
-sub exercise{
-my($age,$BMI)=@_;
-
-while ($BMI) {
+my $age=18;
+my $BMI=22;
  if($BMI<18.9){
 print "you are too light!\n";
 print "Do some basic  exercise and enhanced nutrient intake!\n";
-
-#   if ($age ge 40 || $age le 16 ) {
-# print"jogging for  an hour\.";
-#  }
- #  else {
-#print "Yoga for an hour or jogging for an hour.please choice one.\n";
+   if ($age ge 40 || $age le 16 ) {
+ print"jogging for  an hour\.";
   }
-# }
+  else {
+print "Yoga for an hour or jogging for an hour.please choice one.\n";
+  }
+}
 
 
- if($BMI>24.9){
+
+ elsif ($BMI>24.9){
 print"you are overweight!\n";
 print"Do more exercise to keep health!\n";
+  if ($age ge 40 || $age le 16) {
+ print"Jogging two hours or Yoga two hours\.";
   }
-}
- 
-while($age){
- if ($age>40){
-print "do one hours exercise\n\n";
-}
- if($age<16){
-print "do two hours exercise\n\n";
-}
- else{
-print "do three hours \n\n";
+ else {
+print"Running two hours or swimming two hours \.Please choice one!\n";
   }
  }
+
+ else {
+print"you are standard\.Please keep on!\n";
+ if ($age ge 40 || $age le 16 ) {
+ print"Yoga for one hour or jogging one hour\.Please choice one and you will be healthier.\n";
 }
-# if ($age ge 40 || $age le 16) {
-# print"Jogging two hours or Yoga two hours\.";
- # }
-# else {
-#print"Running two hours or swimming two hours \.Please choice one!\n";
-#  }
- # }
-# }
-#}
-#while (18.9<$BMI<24.9){
-#print"you are standard\.Please keep on!\n";
-# if ($age ge 40 || $age le 16 ) {
-# print"Yoga for one hour or jogging one hour\.Please choice one and you will be healthier.\n";
-# }
-#else {
-#print"Running for one and half an hour or jogging one and half an hour \.Please choice one and you will be healthier.\n";
-# }
-#}
+ else {
+print"Running for one and half an hour or jogging one and half an hour \.Please choice one and you will be healthier.\n";
+ }
+}
 
-#exit;
+exit;
 
-#sub BMI {
-# my ( $heigh, $weigh ) = @_;
-# my ($BMI);
-# $BMI = ( $weigh / ( $heigh * $heigh ) ) ;
-# return $BMI;
-#}
+sub BMI {
+ my ( $heigh, $weigh ) = @_;
+ my ($BMI);
+ $BMI = ( $weigh / ( $heigh * $heigh ) ) ;
+ return $BMI;
+}
 
-#sub age{
-#print "your age:\n";
-#$age = <STDIN>;
-#chomp $age;
-#return $age;
-#}
+sub age{
+}
 
